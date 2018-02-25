597e73f36698faf168821853911b7207b004b2e4
tamelosa <tamelosa@gmail.com>
2018-02-25 16:39:45 +0800

    Major code restructuring to better make use of Clock class

--- Clock.py
+++ Clock.py
@@ -13,6 +13,7 @@ import subprocess
 
 try:
     import argparse
+
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
@@ -47,87 +48,87 @@ def get_credentials():
         flow.user_agent = APPLICATION_NAME
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
+        else:  # Needed only for compatibility with Python 2.6
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
 
 
-def get_events():
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
-
-    now = datetime.datetime.now()
-    day1 = now.replace(hour=0, minute=0, second=0)
-    night1 = now.replace(hour=23, minute=59, second=59)
-    tomorrow = now + datetime.timedelta(days=1)
-    day2 = tomorrow.replace(hour=0, minute=0, second=0)
-    night2 = tomorrow.replace(hour=23, minute=59, second=59)
-
-    day1stamp = day1.timestamp()
-    night1stamp = night1.timestamp()
-    day2stamp = day2.timestamp()
-    night2stamp = night2.timestamp()
-
-    day1utc = datetime.datetime.utcfromtimestamp(day1stamp).isoformat() + 'Z'
-    night1utc = datetime.datetime.utcfromtimestamp(night1stamp).isoformat() + 'Z'
-    day2utc = datetime.datetime.utcfromtimestamp(day2stamp).isoformat() + 'Z'
-    night2utc = datetime.datetime.utcfromtimestamp(night2stamp).isoformat() + 'Z'
-
-    print("Getting today's events")
-    todayEventsResult = service.events().list(
-       calendarId='primary', timeMin=day1utc, timeMax=night1utc, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    todayEvents = todayEventsResult.get('items', [])
-    if not todayEvents:
-        print('No events scheduled today')
-    for event in todayEvents:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
-    print("Getting tomorrow's events")
-    tomorrowEventsResult = service.events().list(
-       calendarId='primary', timeMin=day2utc, timeMax=night2utc, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    tomorrowEvents = tomorrowEventsResult.get('items', [])
-    if not tomorrowEvents:
-        print('No events scheduled tomorrow')
-    for event in tomorrowEvents:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
-
 class Clock:
-
     def __init__(self):
-        self.dateTime = datetime.datetime.now()
-        self.date = datetime.datetime.now().date()
-        self.time = datetime.datetime.now().time()
         self.ping = -1
+        self.date_time = datetime.datetime.now()
+        self.date = self.date_time.date()
+        self.time = self.date_time.time()
+        self.today_events = ""
+        self.tomorrow_events = ""
+
+    def get_events(self):
+        credentials = get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http)
+
+        now = datetime.datetime.now()
+        day1 = now.replace(hour=0, minute=0, second=0)
+        night1 = now.replace(hour=23, minute=59, second=59)
+        tomorrow = now + datetime.timedelta(days=1)
+        day2 = tomorrow.replace(hour=0, minute=0, second=0)
+        night2 = tomorrow.replace(hour=23, minute=59, second=59)
+
+        day1stamp = day1.timestamp()
+        night1stamp = night1.timestamp()
+        day2stamp = day2.timestamp()
+        night2stamp = night2.timestamp()
+
+        day1utc = datetime.datetime.utcfromtimestamp(day1stamp).isoformat() + 'Z'
+        night1utc = datetime.datetime.utcfromtimestamp(night1stamp).isoformat() + 'Z'
+        day2utc = datetime.datetime.utcfromtimestamp(day2stamp).isoformat() + 'Z'
+        night2utc = datetime.datetime.utcfromtimestamp(night2stamp).isoformat() + 'Z'
+
+        today_events_result = service.events().list(
+            calendarId='primary', timeMin=day1utc, timeMax=night1utc, maxResults=10, singleEvents=True,
+            orderBy='startTime').execute()
+        self.today_events = today_events_result.get('items', [])
+
+        tomorrow_events_result = service.events().list(
+            calendarId='primary', timeMin=day2utc, timeMax=night2utc, maxResults=10, singleEvents=True,
+            orderBy='startTime').execute()
+        self.tomorrow_events = tomorrow_events_result.get('items', [])
 
     def get_ping(self):
-        if platform.system() == "Windows":           
-            pingBuffer = subprocess.check_output(["ping", "google.com"])
-            pingStr = pingBuffer.decode()
-            aveStr = "Average = "
-            pingLoc = pingStr.find(aveStr)
-            self.ping = pingStr[pingLoc + len(aveStr):len(pingStr) - 4]
-            print("Your ping is "+self.ping+"ms")
+        if platform.system() == "Windows":
+            ping_buffer = subprocess.check_output(["ping", "google.com"])
+            ping_str = ping_buffer.decode()
+            ave_str = "Average = "
+            ping_loc = ping_str.find(ave_str)
+            self.ping = ping_str[ping_loc + len(ave_str):len(ping_str) - 4]
         else:
-            pingBuffer = subprocess.check_output(["ping",
-            "-c", "4", "-q", "google.com"])
-            pingStr = pingBuffer.decode()
-            splitStr = pingStr.split("/")
-            print("Your average ping is "+splitStr[4])
-
+            ping_buffer = subprocess.check_output(["ping", "-c", "4", "-q", "google.com"])
+            ping_str = ping_buffer.decode()
+            split_str = ping_str.split("/")
+            self.ping = split_str[4]
 
     def update_time(self):
-        self.dateTime = datetime.datetime.now()
+        self.date_time = datetime.datetime.now()
+        self.date = self.date_time.date()
+        self.time = self.date_time.time()
+
+
+def print_events(event_list, day):
+    if (day == "today") | (day == "tomorrow"):
+        print("Getting " + day + "'s events")
+        if not event_list:
+            print('No events scheduled '+day)
+        for event in event_list:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
 
 
 clock = Clock()
 clock.get_ping()
 clock.update_time()
-#print(Clock.dateTime())
-get_events()
+clock.get_events()
+print("ping = ", clock.ping, "datetime = ", clock.date_time)
+print_events(clock.today_events, "today")
+print_events(clock.tomorrow_events, "tomorrow")
+
